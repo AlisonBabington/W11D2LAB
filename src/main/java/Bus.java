5d246f0dfe281ae47abac5d06f0a8510b170019d
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-09 11:59:50 +0100

    class init

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Bus {
+    private String destination;
+    private int capacity;
+    private ArrayList<Person> people;
+
+    public Bus(String destination, int capacity) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.people = new ArrayList<>();
+    }
+
+
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class BusTest {
+
+    Bus bus;
+
+    @Before
+    public void before() {
+        bus = new Bus();
+    }
 }
